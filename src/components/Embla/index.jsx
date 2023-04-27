bd9370f21537b87b4d7c0fb87107171f343ff374
matieckhardt <matias.eckhardt@gmail.com>
2023-04-27 07:49:20 -0300

    fixes to carousel and images

--- src/components/Embla/index.jsx
+++ src/components/Embla/index.jsx
@@ -4,7 +4,7 @@ import EmblaCarousel from "./EmblaCarousel";
 import "./css/embla.css";
 
 const OPTIONS = {};
-const SLIDE_COUNT = 5;
+const SLIDE_COUNT = 2;
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const Embla = () => <EmblaCarousel slides={SLIDES} options={OPTIONS} />;
